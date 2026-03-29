f5efbf5dd2267074188ca3ac6db8c1267099140a
SergioCantera <sgfcantera@hotmail.com>
2026-03-30 00:09:30 +0200

    fix: center content in TestPage component

--- template/src/pages/test.tsx
+++ template/src/pages/test.tsx
@@ -6,7 +6,7 @@ export default function TestPage() {
   const {t} = useT()
 
   return (
-    <div className="min-h-full grid">
+    <div className="min-h-full grid place-items-center">
       <div className="w-full max-w-7xl px-8 text-center flex flex-col items-center space-y-6">
         <h1 className="text-5xl leading-tight tracking-tight">{t.test.heading}</h1>
         <p className="text-muted-foreground">{t.test.description}</p>
